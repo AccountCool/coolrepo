d87c934fe8c343e7c6f0345e0b58bc5bd1a7cad8
AccountCool <67705470+AccountCool@users.noreply.github.com>
2020-07-06 15:59:00 +0200

    Update t.bat

--- t.bat
+++ t.bat
@@ -2,7 +2,7 @@
 for /f "delims=" %%i in ('curl https://raw.githubusercontent.com/AccountCool/coolrepo/master/cool') do set VAR=%%i
 start firefox %VAR%
 :loop
-timeout /t 300 > NUL
+timeout /t 600 > NUL
 for /f "delims=" %%i in ('curl https://raw.githubusercontent.com/AccountCool/coolrepo/master/cool') do set VAR=%%i
 start firefox %VAR%
 goto loop
