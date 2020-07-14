d5b202f67bb60ca7f245355111bed6fd3eef9ffe
AccountCool <67705470+AccountCool@users.noreply.github.com>
2020-07-14 19:14:18 +0200

    Update t.bat

--- t.bat
+++ t.bat
@@ -1,6 +1,6 @@
 @echo off
-start firefox "https://zap563172-1.plesk13.zap-webspace.com/"
+start firefox -private-window "https://zap563172-1.plesk13.zap-webspace.com/"
 :loop
 timeout /t 600 > NUL
-start firefox "https://zap563172-1.plesk13.zap-webspace.com/"
+start firefox -private-window "https://zap563172-1.plesk13.zap-webspace.com/"
 goto loop
